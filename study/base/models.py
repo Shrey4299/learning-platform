f0a8c34e91414dc3a9ef9a0759c7e8840c502373
shrey4299 <shreyansh19101@iiitnr.edu.in>
2023-01-17 12:43:12 +0530

    first

---
+++ study/base/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) :
+        return self.name
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL , null=True) 
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL , null=True) 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True , blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+    
---
+++ study/base/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
+
+    path('', views.home, name="home"),
+    path('room/<int:pk>/', views.room, name= "room" ),
+    path('create-room/', views.createRoom, name= "create-room" ),
+    path('about/', views.about, name= "about" ),
+
+    path('update-room/<int:pk>', views.updateRoom, name= "update-room" ),
+    path('delete-room/<int:pk>', views.deleteRoom, name= "delete-room" ),
+    path('delete-message/<int:pk>', views.deleteMessage, name= "delete-message" ),
+
+
+
+
+
+]
